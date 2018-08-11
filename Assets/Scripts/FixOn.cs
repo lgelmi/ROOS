843567d5ab08b1ad13392610b4567a7b06ec64e6
Valerio Oppedisano <voppe+github@voppe.it>
2018-08-11 17:55:14 +0200

    Piano B

---
+++ Assets/Scripts/FixOn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixOn : MonoBehaviour
+{
+    public GameObject toFollow;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toFollow)
+        {
+            var pos = toFollow.transform.position;
+            this.transform.position = new Vector3(pos.x, pos.y, this.transform.position.z);
+
+            this.GetComponent<Camera>().orthographicSize = toFollow.transform.localScale.x + 5;
+        }
+    }
+}
