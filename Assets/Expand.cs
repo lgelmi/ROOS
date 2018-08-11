69ad28777002351148f140f0660531c2496ca451
Valerio Oppedisano <voppe+github@voppe.it>
2018-08-11 12:07:38 +0200

    First commit

--- Assets/Expand.cs
+++ Assets/Expand.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Expand : MonoBehaviour {
+public class Expand : MonoBehaviour
+{
+    public float speed = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localScale += new Vector3(1 * speed, 1 * speed, 0);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        this.speed = 0;
+    }
 }
---
+++ Assets/MouseFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    public Vector3 mousePosition = Vector3.zero;
+    public float moveSpeed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+}
