42004ba8db74855de9f36d6de7c4781d4a8a2093
Vizio <gelmilorenzo@gmail.com>
2018-08-12 11:26:45 +0200

    Updated MouseFollow.

--- Assets/Scripts/MouseFollow.cs
+++ Assets/Scripts/MouseFollow.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class MouseFollow : MonoBehaviour
 {
-    public Vector3 mousePosition = Vector3.zero;
-    public float moveSpeed = 0.1f;
+    public float maxSpeed = 0.5f;
+    public float minimumDistance = 0;
+    public float maximumDistance = 10;
 
     // Use this for initialization
     void Start()
     {
-        this.FixedUpdate();
+        follow();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        follow();
+    }
+
+    void follow()
+    {
+        float distance = Vector3.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        float speed = maxSpeed * (distance - minimumDistance) / (maximumDistance - minimumDistance);
+        print(distance);
+        print(minimumDistance);
+        print(speed);
+        if (speed < 0)
+            speed = 0;
+        else if (speed > maxSpeed)
+            speed = maxSpeed;
+        Vector3 newPosition = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), speed);
+        //Avoid movement on z axis
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
     }
 }
